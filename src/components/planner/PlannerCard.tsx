263cec611d1fa59ff2ab688d4e98025690fb16b3
Brian <briantcheung@gmail.com>
2025-09-12 12:03:30 -0700

    Revamp planner components

--- src/components/planner/PlannerCard.tsx
+++ src/components/planner/PlannerCard.tsx
@@ -39,6 +39,7 @@ import {
     getHiddenItems,
     getSelectedCharacters,
     getSelectedWeapons,
+    removeItem,
     setPlannerCharacters,
     setPlannerWeapons,
     toggleHidden,
@@ -135,6 +136,7 @@ function PlannerCard({ data }: PlannerCardProps) {
             const newValues = weapons.filter((wep) => wep.name !== name);
             dispatch(setPlannerWeapons(newValues));
         }
+        dispatch(removeItem(data.id));
     };
 
     const iconButtonProps: IconButtonProps = {
--- src/rtk/reducers/planner.ts
+++ src/rtk/reducers/planner.ts
@@ -27,11 +27,13 @@ interface PlannerState {
     totalCost: TotalCostObject;
     characters: CharacterCostObject[];
     weapons: WeaponCostObject[];
+    items: string[];
     hidden: string[];
 }
 
 const storedCharacters = localStorage.getItem("planner/characters") || "null";
 const storedWeapons = localStorage.getItem("planner/weapons") || "null";
+const storedItems = localStorage.getItem("planner/items") || "null";
 const storedHidden = localStorage.getItem("planner/hidden") || "null";
 
 const initialState: PlannerState = {
@@ -57,6 +59,7 @@ const initialState: PlannerState = {
     } as TotalCostObject,
     characters: storedCharacters !== "null" ? JSON.parse(storedCharacters) : [],
     weapons: storedWeapons !== "null" ? JSON.parse(storedWeapons) : [],
+    items: storedItems !== "null" ? JSON.parse(storedItems) : [],
     hidden: storedHidden !== "null" ? JSON.parse(storedHidden) : [],
 };
 
@@ -178,6 +181,15 @@ export const plannerSlice = createSlice({
                 });
             }
         },
+        addItem: (state, action: PayloadAction<string>) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action: PayloadAction<string>) => {
+            state.items.splice(state.items.indexOf(action.payload), 1);
+        },
+        setItems: (state, action: PayloadAction<string[]>) => {
+            state.items = action.payload;
+        },
         toggleHidden: (state, action: PayloadAction<string>) => {
             !state.hidden.includes(action.payload)
                 ? state.hidden.push(action.payload)
@@ -259,6 +271,7 @@ export const plannerSlice = createSlice({
             state.characters,
         getSelectedWeapons: (state): WeaponCostObject[] => state.weapons,
         getTotalCost: (state): TotalCostObject => state.totalCost,
+        getItems: (state): string[] => state.items,
         getHiddenItems: (state): string[] => state.hidden,
     },
 });
@@ -268,6 +281,9 @@ export const {
     setPlannerWeapons,
     updateCharacterCosts,
     updateWeaponCosts,
+    addItem,
+    removeItem,
+    setItems,
     toggleHidden,
 } = plannerSlice.actions;
 
@@ -275,6 +291,7 @@ export const {
     getSelectedCharacters,
     getSelectedWeapons,
     getTotalCost,
+    getItems,
     getHiddenItems,
 } = plannerSlice.selectors;
 
@@ -301,5 +318,13 @@ startAppListening({
     effect: (_, state) => {
         const data = JSON.stringify(state.getState().planner.hidden);
         localStorage.setItem("planner/hidden", data);
+    },
+});
+
+startAppListening({
+    matcher: isAnyOf(addItem, removeItem, setItems),
+    effect: (_, state) => {
+        const data = JSON.stringify(state.getState().planner.items);
+        localStorage.setItem("planner/items", data);
     },
 });
