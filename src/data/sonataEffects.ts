27860cc8bf8f19c15494407310dbc96856f6748e
Brian <briantcheung@gmail.com>
2025-10-06 20:53:54 -0700

    Version 2.7 Update

--- src/data/sonataEffects.ts
+++ src/data/sonataEffects.ts
@@ -121,6 +121,6 @@ export const sonataEffects: SonataEffect[] = [
     {
         name: "Flamewing's Shadow",
         displayName: "Flamewing's Shadow",
-        "3pc": "Dealing Echo Skill DMG increases Heavy Attack Crit. Rate by 20% for 6s. Dealing Heavy Attack DMG increases Echo Skill Crit. Rate by 20% for 6s. While both effects are active, gain 16% Fusion DMG Bonus.",
+        "3pc": 'Dealing Echo Skill DMG increases Heavy Attack Crit. Rate by 20% for 6s. Dealing Heavy Attack DMG increases Echo Skill Crit. Rate by 20% for 6s. While both effects are active, gain 16% <span class="text-fusion">Fusion DMG Bonus</span>.',
     },
 ];
